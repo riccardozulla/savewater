0b327e9f23436e50cbcdf49b1935fd8b34183151
riccardozulla <riccardozulla@gmail.com>
2022-06-30 11:51:37 +0200

    DashboardFragment added to HomeFragment

---
+++ app/src/main/java/it/units/sim/savewater/ui/home/DashboardFragment.java
@@ -0,0 +1,28 @@
+package it.units.sim.savewater.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import it.units.sim.savewater.R;
+
+public class DashboardFragment extends Fragment {
+
+    private static final String TAG = "DashboardFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+}
