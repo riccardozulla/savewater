e775027ef99780d32b75935c11090860d9e90a7f
riccardozulla <riccardozulla@gmail.com>
2022-07-01 17:35:11 +0200

    LoginFragment added

---
+++ app/src/main/java/it/units/sim/savewater/ui/auth/LoginFragment.java
@@ -0,0 +1,31 @@
+package it.units.sim.savewater.ui.auth;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import it.units.sim.savewater.databinding.FragmentLoginBinding;
+
+public class LoginFragment extends Fragment {
+    private FragmentLoginBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        LoginViewModel loginViewModel =
+                new ViewModelProvider(this).get(LoginViewModel.class);
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
