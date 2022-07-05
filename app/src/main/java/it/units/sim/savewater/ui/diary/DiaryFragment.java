ee00ed8c9df21ea1ccc6b2376ff79c9d37c68bec
riccardozulla <riccardozulla@gmail.com>
2022-07-05 12:03:37 +0200

    Date picker added to DiaryFragment

--- app/src/main/java/it/units/sim/savewater/ui/diary/DiaryFragment.java
+++ app/src/main/java/it/units/sim/savewater/ui/diary/DiaryFragment.java
@@ -12,11 +12,20 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.ServerTimestamp;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import it.units.sim.savewater.databinding.FragmentDiaryBinding;
 import it.units.sim.savewater.ui.UtilityAdapter;
@@ -24,8 +33,14 @@ import it.units.sim.savewater.ui.UtilityAdapter;
 public class DiaryFragment extends Fragment implements UtilityAdapter.OnUtilitySelectedListener {
 
     private static final String TAG = "DiaryFragment";
+    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private final FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    private final DateFormat formatter = DateFormat.getDateInstance(DateFormat.MEDIUM);
+    MaterialDatePicker<Long> datePicker;
     private UtilityAdapter mAdapter;
     private FragmentDiaryBinding binding;
+    @ServerTimestamp
+    private Timestamp timestamp;
 
     @Nullable
     @Override
@@ -39,8 +54,19 @@ public class DiaryFragment extends Fragment implements UtilityAdapter.OnUtilityS
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Query query = FirebaseFirestore.getInstance().collection("utilities");
+        if (datePicker == null)
+            datePicker = createDatePicker();
+        timestamp = Timestamp.now();
 
+        binding.editTextDate.setText(formatter.format(timestamp.toDate()));
+        binding.editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePicker.show(getChildFragmentManager(), TAG);
+            }
+        });
+
+        Query query = generateQuery(dateWithoutTime(timestamp.toDate()));
         // RecyclerView
         mAdapter = new UtilityAdapter(query, this) {
             @Override
@@ -60,6 +86,7 @@ public class DiaryFragment extends Fragment implements UtilityAdapter.OnUtilityS
         RecyclerView recyclerView = binding.utilityDiary;
         recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
         recyclerView.setAdapter(mAdapter);
+
 
     }
 
@@ -87,5 +114,49 @@ public class DiaryFragment extends Fragment implements UtilityAdapter.OnUtilityS
     public void onUtilitySelected(DocumentSnapshot utility) {
         Log.w(TAG, "utility selected");
         //TODO: do something
+
+    }
+
+    private Query generateQuery(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        Timestamp startTime = new Timestamp(calendar.getTime());
+        calendar.add(Calendar.DATE, 1);
+        Timestamp endTime = new Timestamp(calendar.getTime());
+
+        return firebaseFirestore.collection("users").
+                document(firebaseAuth.getCurrentUser().getUid()).
+                collection("diary").orderBy("timestamp").
+                startAt(startTime).endAt(endTime);
+
+    }
+
+    private MaterialDatePicker<Long> createDatePicker() {
+        MaterialDatePicker<Long> datePicker =
+                MaterialDatePicker.Builder.datePicker()
+                        .setTitleText("Select date")
+                        .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                        .build();
+
+        datePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener() {
+            @Override
+            public void onPositiveButtonClick(Object selection) {
+                Date date = new Date();
+                date.setTime((Long) selection);
+                mAdapter.setQuery(generateQuery(date));
+                binding.editTextDate.setText(formatter.format(date));
+            }
+        });
+        return datePicker;
+    }
+
+    private Date dateWithoutTime(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
     }
 }
