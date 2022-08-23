e16df08502375c32fb561e468b51ea5c88ac5d2d
riccardozulla <riccardozulla@gmail.com>
2022-08-23 12:56:14 +0200

    HalfPieChartFragment and SharingChartFragment added

---
+++ app/src/main/java/it/units/sim/savewater/ui/dashboard/HalfPieChartFragment.java
@@ -0,0 +1,109 @@
+package it.units.sim.savewater.ui.dashboard;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.units.sim.savewater.databinding.FragmentPieChartBinding;
+import it.units.sim.savewater.model.Utility;
+import it.units.sim.savewater.ui.home.DashboardViewModel;
+
+public class HalfPieChartFragment extends SharingChartFragment<PieChart> {
+
+    private static final String TAG = "HalfPieChartActivity";
+    private DashboardViewModel dashboardViewModel;
+    private FragmentPieChartBinding binding;
+    private int waterTarget;
+
+    @Override
+    protected View drawChart(@NonNull LayoutInflater inflater, @Nullable Bundle savedInstanceState) {
+        binding = FragmentPieChartBinding.inflate(inflater, null, false);
+
+        dashboardViewModel = new ViewModelProvider(requireActivity()).get(DashboardViewModel.class);
+        dashboardViewModel.getTargetWaterConsumption().observe(getViewLifecycleOwner(), this::updateTarget);
+        dashboardViewModel.getDailyWaterConsumption().observe(getViewLifecycleOwner(), this::updateData);
+
+        chart = binding.pieChart;
+        chart.setBackgroundColor(Color.TRANSPARENT);
+        chart.getDescription().setEnabled(false);
+        chart.setUsePercentValues(true);
+
+        chart.setDrawHoleEnabled(true);
+        chart.setHoleColor(Color.TRANSPARENT);
+
+        chart.setTransparentCircleAlpha(0);
+        chart.setHoleRadius(58f);
+        chart.setTransparentCircleRadius(61f);
+        chart.setDrawCenterText(true);
+
+        chart.setRotationEnabled(false);
+        chart.setHighlightPerTapEnabled(true);
+
+        chart.setMaxAngle(240f); // HALF CHART
+        chart.setRotationAngle(150f);
+        chart.setCenterTextColor(Color.rgb(255, 192, 56));
+
+        chart.animateY(1000);
+
+        Legend l = chart.getLegend();
+        l.setEnabled(false);
+
+        chart.setEntryLabelColor(Color.WHITE);
+        chart.setEntryLabelTextSize(12f);
+
+        return binding.getRoot();
+    }
+
+    private void updateData(List<DocumentSnapshot> documentSnapshots) {
+        updatePieChart(documentSnapshots.stream().map(d -> d.toObject(Utility.class).getWaterConsumption()).reduce(0, Integer::sum));
+    }
+
+    private void updateTarget(Integer integer) {
+        waterTarget = integer;
+    }
+
+    private void updatePieChart(int waterConsumption) {
+        ArrayList<PieEntry> values = new ArrayList<>();
+        ArrayList<Integer> colors = new ArrayList<>();
+        if (waterConsumption < waterTarget) {
+            values.add(new PieEntry((float) waterConsumption, "Water used"));
+            values.add(new PieEntry((float) waterTarget - waterConsumption, ""));
+            colors.add(Color.GREEN);
+            colors.add(Color.LTGRAY);
+        } else {
+            values.add(new PieEntry((float) waterConsumption, "Water used"));
+            colors.add(Color.RED);
+        }
+
+        PieDataSet dataSet = new PieDataSet(values, "Water");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+
+        dataSet.setColors(colors);
+        dataSet.setSelectionShift(0f);
+
+        PieData data = new PieData(dataSet);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+        data.setValueTextColor(Color.WHITE);
+        chart.setData(data);
+        chart.setCenterText(waterConsumption + "/" + waterTarget + "\nLiters");
+        chart.invalidate();
+    }
+}
---
+++ app/src/main/java/it/units/sim/savewater/ui/dashboard/SharingChartFragment.java
@@ -0,0 +1,84 @@
+package it.units.sim.savewater.ui.dashboard;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
+import androidx.fragment.app.Fragment;
+
+import com.github.mikephil.charting.charts.Chart;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import it.units.sim.savewater.databinding.FragmentSharingChartBinding;
+
+public abstract class SharingChartFragment<T extends Chart> extends Fragment {
+
+    private static final String TAG = "SharingChartFragment";
+    private FragmentSharingChartBinding binding;
+    protected T chart;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "before super.onCreate");
+        super.onCreateView(inflater, container, savedInstanceState);
+        Log.d(TAG, "after super.onCreate");
+        binding = FragmentSharingChartBinding.inflate(inflater, container, false);
+        binding.fragmentContainerView.addView(drawChart(inflater, savedInstanceState));
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        binding.buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareChart(chart);
+            }
+        });
+    }
+
+    protected abstract View drawChart(@NonNull LayoutInflater inflater, @Nullable Bundle savedInstanceState);
+
+    private Uri saveImage(Bitmap image) {
+        //TODO - Should be processed in another thread
+        File imagesFolder = new File(requireActivity().getCacheDir(), "images");
+        Uri uri = null;
+        try {
+            imagesFolder.mkdirs();
+            File file = new File(imagesFolder, "shared_image.png");
+
+            FileOutputStream stream = new FileOutputStream(file);
+            image.compress(Bitmap.CompressFormat.PNG, 90, stream);
+            stream.flush();
+            stream.close();
+            uri = FileProvider.getUriForFile(requireActivity(), "it.units.sim.savewater", file); //"com.mydomain.fileprovider"
+
+        } catch (IOException e) {
+            Log.d(TAG, "IOException while trying to write file for sharing: " + e.getMessage());
+        }
+        return uri;
+    }
+
+    protected void shareChart(T chart) {
+        Bitmap bitmap = chart.getChartBitmap();
+        Uri bitmapUri = saveImage(bitmap);
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_STREAM, bitmapUri);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.setType("image/png");
+        startActivity(intent);
+    }
+}
