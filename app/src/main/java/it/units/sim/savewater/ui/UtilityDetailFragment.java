5eab9032b9bf6630d0fbe1aa2d873d70a3be2165
riccardozulla <riccardozulla@gmail.com>
2022-08-23 16:02:12 +0200

    code formatted and lambda replaced

--- app/src/main/java/it/units/sim/savewater/ui/UtilityDetailFragment.java
+++ app/src/main/java/it/units/sim/savewater/ui/UtilityDetailFragment.java
@@ -10,16 +10,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import it.units.sim.savewater.R;
 import it.units.sim.savewater.databinding.FragmentUtilityDetailBinding;
 import it.units.sim.savewater.model.Utility;
+import it.units.sim.savewater.utils.FirebaseUtils;
 
 public class UtilityDetailFragment extends Fragment {
 
@@ -29,8 +25,6 @@ public class UtilityDetailFragment extends Fragment {
     public static final int NUMBER_PICKER_MAX_VALUE = 500;
     public static final int NUMBER_PICKER_MIN_VALUE = 0;
     private static final String TAG = "UtilityDetailFragment";
-    private final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-    private final FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     private FragmentUtilityDetailBinding binding;
 
     @Nullable
@@ -54,29 +48,18 @@ public class UtilityDetailFragment extends Fragment {
         binding.editTextUtilityDescription.setText(getArguments().getString(ARG_DESCRIPTION));
 
 
-        binding.buttonUtilityBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.action_utilityDetailFragment_to_utilityFragment);
-            }
-        });
+        binding.buttonUtilityBack.setOnClickListener(v -> Navigation.findNavController(v).navigate(R.id.action_utilityDetailFragment_to_utilityFragment));
 
-        binding.buttonUtilityAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Utility utility = new Utility(binding.editTextUtilityTitle.getText().toString(), binding.editTextUtilityDescription.getText().toString(), binding.numberPickerWaterConsumption.getValue());
-                firebaseFirestore.collection("users").document(firebaseAuth.getUid()).collection("diary").add(utility).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentReference> task) {
-                        if (task.isSuccessful()) {
-                            Snackbar.make(requireView(), "Success", Snackbar.LENGTH_LONG).show();
-                            Navigation.findNavController(v).navigate(R.id.action_utilityDetailFragment_to_utilityFragment);
-                        } else {
-                            Snackbar.make(requireView(), "Failed", Snackbar.LENGTH_LONG).show();
-                        }
-                    }
-                });
-            }
+        binding.buttonUtilityAdd.setOnClickListener(v -> {
+            Utility utility = new Utility(binding.editTextUtilityTitle.getText().toString(), binding.editTextUtilityDescription.getText().toString(), binding.numberPickerWaterConsumption.getValue());
+            FirebaseUtils.utilitiesRef.add(utility).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    Snackbar.make(requireView(), "Success", Snackbar.LENGTH_LONG).show();
+                    Navigation.findNavController(v).navigate(R.id.action_utilityDetailFragment_to_utilityFragment);
+                } else {
+                    Snackbar.make(requireView(), "Failed", Snackbar.LENGTH_LONG).show();
+                }
+            });
         });
     }
 }
