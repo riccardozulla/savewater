817905a1cc9a9b7dc6d05d8a27b3ed8b892492d3
riccardozulla <riccardozulla@gmail.com>
2022-08-23 13:03:57 +0200

    DashboardViewModel added to HomeFragment

--- app/src/main/java/it/units/sim/savewater/ui/home/HomeFragment.java
+++ app/src/main/java/it/units/sim/savewater/ui/home/HomeFragment.java
@@ -7,20 +7,26 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
+
+import java.util.Date;
 
 import it.units.sim.savewater.R;
 import it.units.sim.savewater.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+    private static final String TAG = "HomeFragment";
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        DashboardViewModel dashboardViewModel = new ViewModelProvider(requireActivity()).get(DashboardViewModel.class);
+        dashboardViewModel.setDate(new Date());
 
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
