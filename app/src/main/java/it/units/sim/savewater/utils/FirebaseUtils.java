b5ae1bbeb67b91e43675cb5a1fa1561e6491e80b
riccardozulla <riccardozulla@gmail.com>
2022-08-24 15:05:31 +0200

    possible memory leak removed

--- app/src/main/java/it/units/sim/savewater/utils/FirebaseUtils.java
+++ app/src/main/java/it/units/sim/savewater/utils/FirebaseUtils.java
@@ -2,9 +2,6 @@ package it.units.sim.savewater.utils;
 
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -12,8 +9,6 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class FirebaseUtils {
 
-    private static final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-    private static final FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
     private static final String TAG = "Utils";
 
     public static DocumentReference userRef;
@@ -21,53 +16,38 @@ public class FirebaseUtils {
 
     public static void init() {
         if (isAuthenticated()) {
-            userRef = firebaseFirestore.collection("users").document(firebaseAuth.getCurrentUser().getUid());
+            userRef = FirebaseFirestore.getInstance().collection("users").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
             utilitiesRef = userRef.collection("diary");
         }
     }
 
     public static void signOut() {
         if (isAuthenticated()) {
-            firebaseAuth.signOut();
+            FirebaseAuth.getInstance().signOut();
         }
     }
 
     public static void deleteAccount() {
         if (isAuthenticated()) {
-            firebaseAuth.getCurrentUser().delete()
+            FirebaseAuth.getInstance().getCurrentUser().delete()
                     .onSuccessTask(t -> userRef.delete())
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.w(TAG, "Delete account failed");
-                        }
-                    });
+                    .addOnFailureListener(e -> Log.w(TAG, "Delete account failed"));
         }
     }
 
     public static void sendResetPassword() {
-        firebaseAuth.sendPasswordResetEmail(firebaseAuth.getCurrentUser().getEmail())
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Reset password failed");
-                    }
-                });
+        FirebaseAuth.getInstance().sendPasswordResetEmail(FirebaseAuth.getInstance().getCurrentUser().getEmail())
+                .addOnFailureListener(e -> Log.w(TAG, "Reset password failed"));
     }
 
     public static void changeEmail(String email) {
-        firebaseAuth.getCurrentUser().updateEmail(email)
+        FirebaseAuth.getInstance().getCurrentUser().updateEmail(email)
                 .onSuccessTask(t -> userRef.update("email", email))
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Change email failed");
-                    }
-                });
+                .addOnFailureListener(e -> Log.w(TAG, "Change email failed"));
     }
 
     public static boolean isAuthenticated() {
-        return firebaseAuth.getCurrentUser() != null;
+        return FirebaseAuth.getInstance().getCurrentUser() != null;
 
     }
 }
